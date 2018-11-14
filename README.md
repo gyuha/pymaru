53e3004015abf0f7a8f4b2f3cd13a5a2d383f62c
gyuha <nicegyuha@gmail.com>
2018-11-15 00:51:14 +0900

    pyqt 공부 중

--- README.md
+++ README.md
@@ -19,3 +19,13 @@ pip install -r requirements.txt
 ## Design
 
 ![GUI](./pyMaru.png)
+
+## PyQt links
+### Tutorial
+ - [파이썬으로 배우는 알고리즘 트레이딩](https://wikidocs.net/book/110)
+ - [예제로 배우는 PyQt](https://opentutorials.org/module/544)
+ - [PyQt5 tutorial](http://zetcode.com/gui/pyqt5/)
+
+### PyLint
+ - https://stackoverrun.com/ko/q/12341695
+ - https://stackoverflow.com/questions/46337716/pylint-cant-find-qwidget-and-qapplication
---
+++ label.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'label.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 300)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setMinimumSize(QtCore.QSize(0, 200))
+        self.label.setSizeIncrement(QtCore.QSize(0, 0))
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "출력"))
+        self.pushButton_2.setText(_translate("Dialog", "BUTTON 1"))
+        self.pushButton.setText(_translate("Dialog", "BUTTON 2"))
+        self.pushButton_3.setText(_translate("Dialog", "BUTTON 3"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
